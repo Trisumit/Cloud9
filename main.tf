45ae53c50163126daffa9b2c0d598ba54b3a4c96
Cloud9 <cloud9@192.168.1.5>
2025-07-13 17:21:19 +0530

    formate Updated

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.5.0" # Adjust if needed
+  required_version = ">= 1.5.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.2.0" # Important: specify version here
+      version = "~> 6.2.0"
     }
   }
 }
@@ -24,12 +24,6 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   ingress {
     from_port   = 22
     to_port     = 22
@@ -37,22 +31,28 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "web" {
-  ami             = "ami-0e670eb768a5fc3d4" # Amazon Linux 2 in ap-south-1
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.web_sg.name]
+  ami                         = "ami-0e670eb768a5fc3d4"
+  instance_type               = "t2.micro"
+  security_groups             = [aws_security_group.web_sg.name]
   associate_public_ip_address = aws_instance.web.public_ip != null ? true : false
-  key_name = "sumit-github-key"
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              yum install -y httpd
-              systemctl start httpd
-              systemctl enable httpd
-              echo "<h1>Welcome to Sumeet's Web Server</h1>" > /var/www/html/index.html
-            EOF
+  key_name                    = "sumit-github-key"
+  user_data                   = <<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y httpd
+    systemctl start httpd
+    systemctl enable httpd
+    echo "<h1>Welcome to Sumeet's Web Server</h1>" > /var/www/html/index.html
+  EOF
 
   tags = {
     Name = "TerraformWebInstance"
--- output.tf
+++ output.tf
@@ -1,18 +1,19 @@
 output "public_ip" {
-    description = "Public IP of the web server"
-    value       = aws_instance.web.public_ip
+  description = "Public IP of the web server"
+  value       = aws_instance.web.public_ip
 }
+
 output "instance_id" {
-    description = "Instance ID of the web server"
-    value       = aws_instance.web.id
-  
+  description = "Instance ID of the web server"
+  value       = aws_instance.web.id
 }
+
 output "security_group_id" {
-    description = "Security Group ID for the web server"
-    value       = aws_security_group.web_sg.id
+  description = "Security Group ID for the web server"
+  value       = aws_security_group.web_sg.id
 }
+
 output "public_dns" {
-    description = "Public DNS of the web server"
-    value       = aws_instance.web.public_dns
-  
+  description = "Public DNS of the web server"
+  value       = aws_instance.web.public_dns
 }
